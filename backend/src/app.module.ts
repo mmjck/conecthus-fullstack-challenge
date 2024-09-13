2d0b329c754792d0afba831888475171d0aa5be4
mmjck <msantosjck@gmail.com>
2024-09-13 12:05:30 -0400

    added database connection

---
+++ backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'users',
+      password: 'password',
+      username: 'admin',
+      logging: true,
+      port: 5433,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
