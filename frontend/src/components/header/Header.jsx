e99448569a3a30efefa1459dfd6ab0efa0bcf69a
mmjck <msantosjck@gmail.com>
2024-09-13 12:48:14 -0400

    feat: added header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="grid h-[50px] bg-white content-center justify-items-end px-10 py-10">
+      <div className="flex items-center justify-center ">
+        <div className="flex items-center justify-center bg-black border-2 border-green rounded-full h-10 w-10">
+          <span className="text-white text-lg">MS</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
