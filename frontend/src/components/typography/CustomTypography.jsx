140e9f6af17ea2835b934dc0f40df34b6547291f
mmjck <msantosjck@gmail.com>
2024-09-13 12:50:38 -0400

    feat: added custom typography

---
+++ frontend/src/components/typography/CustomTypography.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomTypography = ({ label }) => {
+  return (
+    <span className="ml-5 font-normal text-[24px] font-manrope text-[#0B2B25] tracking-normal">
+      {label}
+    </span>
+  );
+};
+
+export default CustomTypography;
