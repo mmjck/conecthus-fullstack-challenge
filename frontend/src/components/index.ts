e965e33b2760180c2ce157f7cbfedb2c652f2eb6
mmjck <msantosjck@gmail.com>
2024-09-13 12:47:13 -0400

    feat: export components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,32 @@
+import Search from "./search/Search";
+import EmptyState from "./empty-state/EmptyState";
+import PrimaryButton from "./button/PrimaryButton"
+import SecondaryButton from "./button/SecondaryButton"
+import SuccessPopUp from "./pop-up/SuccessPopup"
+
+import Header from "./header/Header";
+import ConfirmDialog from "./dialog/ConfirmDialog";
+
+import ErrorPopUp from "./pop-up/ErrorPopup"
+import CustomTypography from "./typography/CustomTypography"
+import Breadcrumb from "./breadcrumb/Breadcrumb"
+import Input from "./input/Input";
+import SideBarMenu from "./side/SideBarMenu"
+
+import SideView from "./side/SideView"
+
+export {
+    Header,
+    Search,
+    EmptyState,
+    PrimaryButton,
+    SecondaryButton,
+    SuccessPopUp,
+    ErrorPopUp,
+    ConfirmDialog,
+    Breadcrumb,
+    SideView,
+    SideBarMenu,
+    CustomTypography,
+    Input
+}
